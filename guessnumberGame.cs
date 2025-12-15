fe2e0f61c4c500815ac8c8f32c19ee3be290ba2e
Ali Zamani <166255379+AliZamani696@users.noreply.github.com>
2025-12-15 10:25:03 +0330

    Add files via upload

---
+++ guessnumberGame.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System;
+
+
+Console.WriteLine("i select my number ");
+Random PCnum = new Random();
+int myNum = PCnum.Next(50);
+bool running = true;
+uint RemainingOpportunity = 5;
+while (running)
+{
+    Console.WriteLine("whats your guess??");
+    var input = Console.ReadLine();
+    int j = int.Parse(input);
+    if (j > myNum &&  RemainingOpportunity != 0)
+    {
+
+        Console.WriteLine($"your guess is large RemainingOpportunity: {RemainingOpportunity}");
+        RemainingOpportunity--;
+    }
+    else if( j < myNum && RemainingOpportunity !=0)
+    {
+        Console.WriteLine($"my number is large RemainingOpportunity: {RemainingOpportunity}");
+        RemainingOpportunity--;
+    }
+    else if(j == myNum)
+    {
+        Console.WriteLine($"you win myNum is {myNum} your guess{j} ");
+        running = false;
+    }
+    else if(RemainingOpportunity ==0)
+    {
+        Console.WriteLine("your opportuntity is end! you lose");
+        running = false;
+    }
+}
