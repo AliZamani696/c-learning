dc0e2541c35fb259968e181998d37a20379f03e7
Ali Zamani <166255379+AliZamani696@users.noreply.github.com>
2025-12-15 07:55:03 +0330

    Add files via upload

---
+++ DayOfWeek.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System;
+
+
+
+public enum dayOfWeek:byte{
+    Sunday,      // 0
+    Monday,      // 1
+    Tuesday,     // 2
+    Wednesday,   // 3
+    Thursday,    // 4
+    Friday,      // 5
+    Saturday    // 6 
+}
+
+
+class App{
+    static void Main(){
+        dayOfWeek today = dayOfWeek.Monday;
+        dayOfWeek intday = (dayOfWeek)5;
+        Console.WriteLine(intday);
+    }
+}
