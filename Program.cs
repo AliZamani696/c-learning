2b78c474c19d0a58a54feb254f2b5c1f3f95f392
Ali Zamani <166255379+AliZamani696@users.noreply.github.com>
2025-12-16 22:14:38 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+
+
+
+
+namespace App
+{
+    public class MainApp
+    {
+        static public bool RevversInt(int num)
+        {
+            int NewNum = num;
+            int res = 0;
+            while(NewNum>0)
+            {
+                res = res *10 + NewNum %10;
+                NewNum /= 10;
+            }
+            if(res == num)
+                return true;
+            else
+                return false;
+        }
+        static public void Main()
+        {
+            int num =-121;
+            Console.WriteLine(RevversInt(num));
+
+        }
+    }
+}
