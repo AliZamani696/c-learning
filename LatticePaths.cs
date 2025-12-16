ee13315d2434af43b48822184d3d11b3ea2174c3
Ali Zamani <166255379+AliZamani696@users.noreply.github.com>
2025-12-16 10:00:41 +0330

    Add files via upload

---
+++ LatticePaths.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+using System;
+using System.Numerics;
+
+
+namespace Program
+{
+   class MyApp
+    {
+        public static void Main()
+        {
+            int grid1 = 20;
+            int grid2 = 20;
+            int TotalSteps = grid1 + grid2;
+            BigInteger result = 1;
+            for(int j=1;j <= grid1;j++){
+                result *= (TotalSteps - j +1);
+                result /= j;
+            }
+            Console.WriteLine(result);    
+        }
+    }
+}
